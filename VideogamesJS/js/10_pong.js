8f51ca9a40da9818e6af15ece5e7a3fd84161350
sant-mell <sant.mell016@gmail.com>
2026-04-20 14:00:30 -0600

    fixed pong sideways bounce

--- VideogamesJS/js/10_pong.js
+++ VideogamesJS/js/10_pong.js
@@ -1,80 +1,81 @@
-/*
- * Detection of collisions between boxes
- *
- * Gilberto Echeverria
- * 2025-03-13
- */
-
 "use strict";
-
-// Global variables
 const canvasWidth = 800;
 const canvasHeight = 600;
+const paddleSpeed = 0.45;
+const ballSpeed = 0.35;
+const paddleWidth = 18;
+const paddleHeight = 110;
+const ballSize = 16;
 
-// Context of the Canvas
 let ctx;
-
-// A variable to store the game object
 let game;
-
-// Variable to store the time at the previous frame
 let oldTime;
 
-let playerSpeed = 100;
+function aabbOverlap(obj1, obj2) {
+    const left1 = obj1.position.x - obj1.halfSize.x;
+    const right1 = obj1.position.x + obj1.halfSize.x;
+    const top1 = obj1.position.y - obj1.halfSize.y;
+    const bottom1 = obj1.position.y + obj1.halfSize.y;
 
-// Class for the main character in the game
+    const left2 = obj2.position.x - obj2.halfSize.x;
+    const right2 = obj2.position.x + obj2.halfSize.x;
+    const top2 = obj2.position.y - obj2.halfSize.y;
+    const bottom2 = obj2.position.y + obj2.halfSize.y;
+
+    return left1 < right2 && right1 > left2 && top1 < bottom2 && bottom1 > top2;
+}
+
 class Paddle extends GameObject {
-    constructor(position, width, height, color, sheetCols) {
-        super(position, width, height, color, "player", sheetCols);
+    constructor(position, color) {
+        super(position, paddleWidth, paddleHeight, color, "paddle");
         this.velocity = new Vector(0, 0);
-
-        this.motion = {
-            up: {
-                axis: "y",
-                sign: -1,
-            },
-            down: {
-                axis: "y",
-                sign: 1,
-            }
-        }
-
-        // Keys pressed to move the player
         this.keys = [];
     }
 
     update(deltaTime) {
-        // Restart the velocity
-        this.velocity.x = 0;
         this.velocity.y = 0;
-        // Modify the velocity according to the directions pressed
+
         for (const direction of this.keys) {
-            const axis = this.motion[direction].axis;
-            const sign = this.motion[direction].sign;
-            this.velocity[axis] += sign;
+            if (direction == "up") {
+                this.velocity.y -= 1;
+            } else if (direction == "down") {
+                this.velocity.y += 1;
+            }
         }
-        // TODO: Normalize the velocity to avoid greater speed on diagonals
 
-        this.position = this.position.plus(this.velocity.times(deltaTime));
-
+        this.position = this.position.plus(this.velocity.times(paddleSpeed * deltaTime));
         this.clampWithinCanvas();
     }
 
     clampWithinCanvas() {
-        if (this.position.y < 0) {
-            this.position.y = 0;
-        } else if (this.position.y + this.height > canvasHeight) {
-            this.position.y = canvasHeight - this.height;
-        } else if (this.position.x < 0) {
-            this.position.x = 0;
-        } else if (this.position.x + this.width > canvasWidth) {
-            this.position.x = canvasWidth - this.width;
+        if (this.position.y - this.halfSize.y < 0) {
+            this.position.y = this.halfSize.y;
+        } else if (this.position.y + this.halfSize.y > canvasHeight) {
+            this.position.y = canvasHeight - this.halfSize.y;
         }
     }
 }
 
+class Ball extends GameObject {
+    constructor(position) {
+        super(position, ballSize, ballSize, "black", "ball");
+        this.velocity = new Vector(0, 0);
+    }
 
-// Class to keep track of all the events and objects in the game
+    launch(direction = 1) {
+        this.velocity = new Vector(direction * ballSpeed, 0);
+    }
+
+    update(deltaTime) {
+        this.position = this.position.plus(this.velocity.times(deltaTime));
+    }
+
+    reset(direction = 1) {
+        this.position = new Vector(canvasWidth / 2, canvasHeight / 2);
+        this.launch(direction);
+    }
+}
+
 class Game {
     constructor() {
         this.createEventListeners();
@@ -82,73 +83,83 @@ class Game {
     }
 
     initObjects() {
-        this.paddleLeft = new Paddle(new Vector(50, canvasHeight / 2 - 30), 40, 100, "red");
-        this.paddleRight = new Paddle(new Vector(canvasWidth - 50, canvasHeight / 2 - 30), 40, 100, "blue");
-
-        this.actors = [];
-        for (let i=0; i<10; i++) {
-            this.addBox();
-        }
+        this.paddleLeft = new Paddle(new Vector(40, canvasHeight / 2), "red");
+        this.paddleRight = new Paddle(new Vector(canvasWidth - 40, canvasHeight / 2), "blue");
+        this.ball = new Ball(new Vector(canvasWidth / 2, canvasHeight / 2));
+        this.scoreLeft = 0;
+        this.scoreRight = 0;
+        this.ball.launch(1);
     }
 
     draw(ctx) {
-        for (let actor of this.actors) {
-            actor.draw(ctx);
-        }
+        this.drawScore(ctx);
         this.paddleLeft.draw(ctx);
         this.paddleRight.draw(ctx);
+        this.ball.draw(ctx);
+    }
+
+    drawScore(ctx) {
+        ctx.fillStyle = "black";
+        ctx.font = "24px monospace";
+        ctx.textAlign = "center";
+        ctx.fillText(`${this.scoreLeft} : ${this.scoreRight}`, canvasWidth / 2, 36);
     }
 
     update(deltaTime) {
-        // Move the player
         this.paddleLeft.update(deltaTime);
         this.paddleRight.update(deltaTime);
+        this.ball.update(deltaTime);
 
-        // Check collision against other objects
-        for (let actor of this.actors) {
-          //  if (boxOverlap(this.paddleLeft, actor)) {
-          //      actor.color = "yellow";
-           // } else {
-           //     actor.color = "grey";
-           // }
+        this.resolvePaddleCollision(this.paddleLeft, 1);
+        this.resolvePaddleCollision(this.paddleRight, -1);
+        this.checkScore();
+    }
+
+    resolvePaddleCollision(paddle, directionOut) {
+        if (!aabbOverlap(this.ball, paddle)) {
+            return;
+        }
+
+        this.ball.position.x = paddle.position.x + directionOut * (paddle.halfSize.x + this.ball.halfSize.x + 1);
+        this.ball.velocity.x *= -1;
+    }
+
+    checkScore() {
+        if (this.ball.position.x + this.ball.halfSize.x < 0) {
+            this.scoreRight += 1;
+            this.ball.reset(1);
+        } else if (this.ball.position.x - this.ball.halfSize.x > canvasWidth) {
+            this.scoreLeft += 1;
+            this.ball.reset(-1);
         }
     }
 
-    addBox() {
-        // TODO: Use the randomRange function to make these values different
-        // Create boxes with minimum size 50, and up to 50 pixels more
-        const size = 50;
-        // Define a random position for the box, within the canvas
-        const posX = 60;
-        const posY = 70;
-        const box = new GameObject(new Vector(posX, posY), size, size, "grey");
-        // Set a property to indicate if the box should be destroyed or not
-        box.destroy = false;
-        this.actors.push(box);
-    }
-
     createEventListeners() {
-        window.addEventListener('keydown', (event) => {
-            if (event.key == 'w') {
-                this.addKey(this.paddleLeft, 'up');
-            } else if (event.key == 's') {
-                this.addKey(this.paddleLeft, 'down');
-            } else if (event.key == 'ArrowUp') {
-                this.addKey(this.paddleRight, 'up');
-            } else if (event.key == 'ArrowDown') {
-                this.addKey(this.paddleRight, 'down');
+        window.addEventListener("keydown", event => {
+            if (event.key == "w") {
+                this.addKey(this.paddleLeft, "up");
+            } else if (event.key == "s") {
+                this.addKey(this.paddleLeft, "down");
+            } else if (event.key == "ArrowUp") {
+                event.preventDefault();
+                this.addKey(this.paddleRight, "up");
+            } else if (event.key == "ArrowDown") {
+                event.preventDefault();
+                this.addKey(this.paddleRight, "down");
             }
         });
 
-        window.addEventListener('keyup', (event) => {
-            if (event.key == 'w') {
-                this.delKey(this.paddleLeft, 'up');
-            } else if (event.key == 's') {
-                this.delKey(this.paddleLeft, 'down');
-            } else if (event.key == 'ArrowUp') {
-                this.delKey(this.paddleRight, 'up');
-            } else if (event.key == 'ArrowDown') {
-                this.delKey(this.paddleRight, 'down');
+        window.addEventListener("keyup", event => {
+            if (event.key == "w") {
+                this.delKey(this.paddleLeft, "up");
+            } else if (event.key == "s") {
+                this.delKey(this.paddleLeft, "down");
+            } else if (event.key == "ArrowUp") {
+                event.preventDefault();
+                this.delKey(this.paddleRight, "up");
+            } else if (event.key == "ArrowDown") {
+                event.preventDefault();
+                this.delKey(this.paddleRight, "down");
             }
         });
     }
@@ -166,34 +177,24 @@ class Game {
     }
 }
 
-
-// Starting function that will be called from the HTML page
 function main() {
-    // Get a reference to the object with id 'canvas' in the page
-    const canvas = document.getElementById('canvas');
-    // Resize the element
+    const canvas = document.getElementById("canvas");
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
-    // Get the context for drawing in 2D
-    ctx = canvas.getContext('2d');
-
-    // Create the game object
+    ctx = canvas.getContext("2d");
     game = new Game();
-
     drawScene(0);
 }
 
-
-// Main loop function to be called once per frame
 function drawScene(newTime) {
-    // Compute the time elapsed since the last frame, in milliseconds
-    let deltaTime = 10;
+    if (oldTime === undefined) {
+        oldTime = newTime;
+    }
 
-    // Clean the canvas so we can draw everything again
+    const deltaTime = Math.min(newTime - oldTime, 35);
+
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
     game.update(deltaTime);
-
     game.draw(ctx);
 
     oldTime = newTime;
